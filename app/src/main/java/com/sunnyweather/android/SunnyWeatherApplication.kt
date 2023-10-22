980b1c550379cbf416dee9ca0266dc31d7e24fa3
Freeman <t961735889@163.com>
2023-10-22 17:26:09 +0800

    上传图片资源、样例登录代码

---
+++ app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
@@ -0,0 +1,22 @@
+package com.sunnyweather.android
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+
+class SunnyWeatherApplication : Application() {
+
+    companion object {
+        @SuppressLint("StaticFieldLeak")
+        private lateinit var context: Context
+
+        fun getContext(): Context {
+            return context
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
+}
